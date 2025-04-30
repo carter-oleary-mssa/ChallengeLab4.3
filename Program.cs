6a1a6bb861297d93faa46cfb419cb906697845c2
carter-oleary <carter.m.oleary@outlook.com>
2025-04-30 11:23:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿char[] s = "Hannah".ToCharArray();
+ReverseString(s);
+Console.WriteLine(s.ToString();
+
+void ReverseString(char[] s)
+{
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        char c = s[i];
+        s[i] = s[s.Length - 1 - i];
+        s[s.Length - 1 - i] = c;
+    }
+}
